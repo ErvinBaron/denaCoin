765b8cedd067699dde7f5a74ea6a231c6331cf04
davidtyu77 <davidtyurikov2023@gmail.com>
2025-01-12 17:53:18 +0200

    also transfer class

--- server/src/Main.java
+++ server/src/Main.java
@@ -51,7 +51,6 @@ public class Main {
 			server.createContext("/register", new CorsHandle(new RegisterHandler()));
 			server.createContext("/get-key", new CorsHandle(new KeyHandler()));
 			server.createContext("/getBalance", new CorsHandle(new GetBalanceHandler()));
-			server.createContext("/transfer", new CorsHandle(new TransferHandler()));
 			server.setExecutor(null); // Default executor
 			server.start();
 
